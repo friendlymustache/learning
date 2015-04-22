2844076d8a6b70d47bcd180a9a9befd63b748c3f
Siddharth Murching <smurching@caltech.edu>
2015-04-22 00:05:34 -0700

    Commit code needed to host hand-curated links

--- webapp/public-src/config/environment.js
+++ webapp/public-src/config/environment.js
@@ -44,7 +44,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    // ENV.host = 'http://cumulus-beta.herokuapp.com';
+    ENV.host = 'http://cumulus-beta.herokuapp.com';
   }
 
   if (environment === 'staging') {
